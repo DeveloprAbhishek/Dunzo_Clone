60e2d1d758b64c6b0e5c65902242ef33dfdfc80b
DeveloprAbhishek <developr.abhishek@gmail.com>
2021-08-23 21:10:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dunzoclone/Activities/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dunzoclone.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dunzoclone.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
