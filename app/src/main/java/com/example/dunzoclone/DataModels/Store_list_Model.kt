5fac69569395a5b6281c99a2983a7839e19c0d75
Akash Kumar Prajapat <rkpsx7@gmail.com>
2021-08-25 00:06:01 +0530

    Created Store Model data class

--- app/src/main/java/com/example/dunzoclone/DataModels/Store_list_Model.kt
+++ app/src/main/java/com/example/dunzoclone/DataModels/Store_list_Model.kt
@@ -1,17 +1,17 @@
 package com.example.dunzoclone.DataModels
 
 class Store_list_Model {
-//    var image_url : String
-//    val store_name : String
-//    var distance : String
-//    var time : String
-//    var address : String
-//
-//    constructor(image_url: String, store_name: String, distance: String, time: String,address : String) {
-//        this.image_url = image_url
-//        this.store_name = store_name
-//        this.distance = distance
-//        this.time = time
-//        this.address = address
-//    }
+    var image_url : String
+    val store_name : String
+    var distance : String
+    var time : String
+    var address : String
+
+    constructor(image_url: String, store_name: String, distance: String, time: String,address : String) {
+        this.image_url = image_url
+        this.store_name = store_name
+        this.distance = distance
+        this.time = time
+        this.address = address
+    }
 }
