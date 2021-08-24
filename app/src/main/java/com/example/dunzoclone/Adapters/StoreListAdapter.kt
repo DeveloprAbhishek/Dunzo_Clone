c3b6fa0b3909794155d4b0bf1ec93d7f351c1e5c
Akash Kumar Prajapat <rkpsx7@gmail.com>
2021-08-25 00:07:26 +0530

    implemented Store Adapter class

--- app/src/main/java/com/example/dunzoclone/Adapters/StoreListAdapter.kt
+++ app/src/main/java/com/example/dunzoclone/Adapters/StoreListAdapter.kt
@@ -7,20 +7,20 @@ import com.example.dunzoclone.DataModels.Store_list_Model
 import com.example.dunzoclone.R
 import com.example.dunzoclone.ViewHolders.StoreViewHolder
 
-//class StoreListAdapter(private val storeList: List<Store_list_Model>) :
-//    RecyclerView.Adapter<StoreViewHolder>() {
-////    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoreViewHolder {
-////        val view =
-////            LayoutInflater.from(parent.context).inflate(R.layout.store_item_layout, parent, false)
-////        return StoreViewHolder(view)
-////    }
-////
-////    override fun onBindViewHolder(holder: StoreViewHolder, position: Int) {
-////        val store: Store_list_Model = storeList[position]
-////        holder.setData(store)
-////    }
-////
-////    override fun getItemCount(): Int {
-////        return storeList.size
-////    }
-//}
+class StoreListAdapter(private val storeList: List<Store_list_Model>) :
+    RecyclerView.Adapter<StoreViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoreViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.store_item_layout, parent, false)
+        return StoreViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StoreViewHolder, position: Int) {
+        val store: Store_list_Model = storeList[position]
+        holder.setData(store)
+    }
+
+    override fun getItemCount(): Int {
+        return storeList.size
+    }
+}
