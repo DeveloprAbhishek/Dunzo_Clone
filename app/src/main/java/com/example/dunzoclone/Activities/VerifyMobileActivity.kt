39fdac96e41f5ffc048709c0b250833c0f274f3d
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-08-24 10:12:56 +0530

    Added LoginScreen and Otp Verify Screen

--- app/src/main/java/com/example/dunzoclone/Activities/VerifyMobileActivity.kt
+++ app/src/main/java/com/example/dunzoclone/Activities/VerifyMobileActivity.kt
@@ -3,10 +3,12 @@ package com.example.dunzoclone.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dunzoclone.R
+import kotlinx.android.synthetic.main.activity_verify_mobile.*
 
 class VerifyMobileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verify_mobile)
+
     }
 }
