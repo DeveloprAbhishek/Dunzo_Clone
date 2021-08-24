b0812f3526c276b10123f62f74ad037bf05d258d
DeveloprAbhishek <developr.abhishek@gmail.com>
2021-08-24 17:14:12 +0530

    home page

--- app/src/main/java/com/example/dunzoclone/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/dunzoclone/Activities/HomeActivity.kt
@@ -8,5 +8,6 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        supportActionBar?.hide()
     }
 }
---
+++ app/src/main/java/com/example/dunzoclone/Fragments/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.example.dunzoclone.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.dunzoclone.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
