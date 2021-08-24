e213487e7f5b16864515efea904a7e1a93632616
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-08-24 18:56:40 +0530

    Dunzo Cash _ Refferal Fragment

--- app/src/main/java/com/example/dunzoclone/Activities/UserProfileActivity.kt
+++ app/src/main/java/com/example/dunzoclone/Activities/UserProfileActivity.kt
@@ -8,6 +8,5 @@ class UserProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
-
     }
 }
