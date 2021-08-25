6e7bcf3666c50677be7c641ff21fae30fac18635
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-08-25 09:11:09 +0530

    Added Recyclerview and Adapter for Store category and Best seller

---
+++ app/src/main/java/com/example/dunzoclone/Adapter/StoreCategoryAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.dunzoclone.Adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dunzoclone.ViewHolder.StoreCategoryViewHolder
+
+class StoreCategoryAdapter : RecyclerView.Adapter<StoreCategoryViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoreCategoryViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: StoreCategoryViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/dunzoclone/ViewHolder/BestSellerViewHolder.kt
@@ -0,0 +1,11 @@
+package com.example.dunzoclone.ViewHolder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class BestSellerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun setData(){
+
+    }
+}
