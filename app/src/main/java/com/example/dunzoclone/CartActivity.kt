b381364468f672cbc811b36117c6791e6a070f60
Amol Pardeshi <pardeshiamol772@gmail.com>
2021-08-25 17:22:10 +0530

    Added cart layout and recyclerview

---
+++ app/src/main/java/com/example/dunzoclone/CartActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dunzoclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
