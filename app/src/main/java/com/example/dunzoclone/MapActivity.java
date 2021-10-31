cdc1b3c2695ccd27e89550d826c4fe87e817ca45
AmolPardeshi99 <pardeshiamol772@gmail.com>
2021-10-31 14:28:03 +0530

    Updated the ACTIVTY

--- app/src/main/java/com/example/dunzoclone/MapActivity.java
+++ app/src/main/java/com/example/dunzoclone/MapActivity.java
@@ -18,6 +18,7 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
     }
 
 
